8e56857bef42ba67c4c4dcfa45deb549c34314ef
Matyas Urban <urban.matyas@icloud.com>
2024-12-31 13:38:40 +0100

    try screen again

--- start_node.sh
+++ start_node.sh
@@ -33,9 +33,18 @@ else
   log_filter=""
 fi
 
-screen -S node-session -dm bash -c "python3 node.py; screen -S node-session -X quit"
-screen -S node-session -X screen bash -c "tail -f node.log | grep $log_filter --line-buffered; screen -S node-session -X quit"
+# Create a named screen session
+screen -S node-session -dm bash -c "python3 node.py; read -p 'Python process ended. Press Enter to close...'"
+
+# Add a new window for log-tailing
+screen -S node-session -X screen bash -c "tail -f node.log | grep $log_filter --line-buffered; read -p 'Log tailing ended. Press Enter to close...'"
+
+# Split the screen horizontally
 screen -S node-session -X split -v
+
+# Focus on the top region (Window 0) for Python CLI
 screen -S node-session -X focus
 screen -S node-session -X select 0
+
+# Reattach to the session
 screen -r node-session
