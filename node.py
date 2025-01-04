feaef5eb60a95ea0435431bbb343047ddcb7fc84
Matyas Urban <urban.matyas@icloud.com>
2025-01-04 14:50:54 +0100

    leave

--- node.py
+++ node.py
@@ -345,7 +345,7 @@ class Node:
                         new_topology = self.topology
                         new_topology.remove(self.id)
                         self.log(logging.INFO, 'Initiating removal of ourselves from the topology.')
-                        self.build_and_enqueue_message(self.successor_id, 'TOPOLOGY_UPDATE', new_topology)
+                        self.build_and_enqueue_message(self.successor_id, 'TOPOLOGY_UPDATE', json.dumps(new_topology))
                 elif command == "status":
                     self.status()
                 elif command == "delay":
